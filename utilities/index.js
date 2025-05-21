d59685c7b00ba2e569884e1ad27310ccbc34568b
trystandj <trystandj@gmail.com>
2025-05-20 20:29:20 -0400

    Error handeling

--- utilities/index.js
+++ utilities/index.js
@@ -25,7 +25,7 @@ Util.getNav = async function (req, res, next) {
   return list
 }
 
-module.exports = Util
+
 
 /* **************************************
 * Build the classification view HTML
@@ -59,3 +59,12 @@ Util.buildClassificationGrid = async function(data){
   }
   return grid
 }
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+module.exports = Util
