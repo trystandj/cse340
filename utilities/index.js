51993f98d0af1f72043afad937e9e464cbd6f26a
trystandj <trystandj@gmail.com>
2025-05-24 07:36:19 -0400

    fixed errors in html and css

--- utilities/index.js
+++ utilities/index.js
@@ -40,9 +40,9 @@ Util.buildClassificationGrid = async function(data){
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+      +' on CSE Motors"></a>'
       grid += '<div class="namePrice">'
-      grid += '<hr />'
+      grid += '<hr>'
       grid += '<h2>'
       grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
       + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
@@ -75,7 +75,7 @@ Util.buildDetailGrid = async function (data){
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_image 
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+      +' on CSE Motors"></a>'
       grid += '<div class="namePrice">'
       grid += '<div class="detailSection">'
       grid += '<h2>'
@@ -87,11 +87,6 @@ Util.buildDetailGrid = async function (data){
       grid += '<span><strong>Color:</strong> ' + vehicle.inv_color + '</span>';
       grid += '<span><strong>Miles:</strong> ' + new Intl.NumberFormat('en-US').format(vehicle.inv_miles)   + '</span>';
       grid += '</div>'
-
-     
-      
-
-      
       grid += '</div>'
       grid += '</div>'
       
